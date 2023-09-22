777875032435c4ac9fd59234846117382ff9100a
Igor <igorlfs@ufmg.br>
2023-09-21 22:07:03 -0300

    feat: instâncias não euclidianas usando `trunc`

--- src/algorithms/algorithms.py
+++ src/algorithms/algorithms.py
@@ -1,9 +1,13 @@
+import time as ti
 from dataclasses import dataclass
+from pathlib import Path
 
 import networkx as nx
 import numpy as np
+import tsplib95
+from scipy.spatial import distance
 
-from src.algorithms.helpers import monte_carlo_step
+from .helpers import monte_carlo_step
 
 # Hiperparâmetros do simulated annealing
 MONTE_CARLO_STEPS = 20000
@@ -16,6 +20,26 @@ class Algorithm:
     name: str
 
 
+def get_time_and_cost(problem_path: Path, data: list[list]) -> None:
+    """Calcula o tempo e o custo de aproximar a instância do TSP em `problem_path`,
+    usando difentes algoritmos e adicionando os resultados em `data`."""
+    problem = tsplib95.load(problem_path)
+    coords = np.array([problem.node_coords[i] for i in problem.get_nodes()])
+    distances = distance.cdist(coords, coords, "euclidean")
+    if problem_path.name.find("att") != -1:
+        distances = np.trunc(distances)  # corte nas instâncias não euclidianas
+    graph = nx.Graph(distances)
+    algorithms = ["Christofides", "TATT", "Simulated Annealing"]
+    for algorithm in algorithms:
+        start = ti.time()
+        result = match_algorithm(algorithm, graph)
+        end = ti.time()
+        time = end - start
+        if result is not None:
+            cost, _ = result
+            data.append([problem.name, algorithm, time, cost])
+
+
 def match_algorithm(name: str, graph: nx.Graph) -> tuple[float, list[int]] | None:
     """Casa um nome com uma função de aproximação para o TSP."""
     match name:
@@ -23,8 +47,8 @@ def match_algorithm(name: str, graph: nx.Graph) -> tuple[float, list[int]] | Non
             return twice_around_the_tree(graph)
         case "Christofides":
             return christofides(graph)
-        case "MonteCarlo":
-            return monte_carlo(graph, 3 * graph.number_of_nodes())
+        case "Simulated Annealing":
+            return simulated_annealing(graph, 3 * graph.number_of_nodes())
 
 
 def twice_around_the_tree(graph: nx.Graph) -> tuple[float, list[int]]:
@@ -52,7 +76,9 @@ def christofides(graph: nx.Graph) -> tuple[float, list[int]]:
     return nx.path_weight(graph, cycle, "weight"), cycle
 
 
-def monte_carlo(graph: nx.Graph, initial_temperature: float) -> tuple[float, list[int]]:
+def simulated_annealing(
+    graph: nx.Graph, initial_temperature: float
+) -> tuple[float, list[int]]:
     """Aproxime o caixeiro viajante para `graph` usando `Simulated Annealing`."""
     distances = nx.to_numpy_matrix(graph)
     path = np.arange(len(distances))
--- src/app.py
+++ src/app.py
@@ -1,33 +1,18 @@
-import time as ti
 from pathlib import Path
 
-import networkx as nx
-import numpy as np
 import pandas as pd
-import tsplib95
-from scipy.spatial import distance
 
-from src.algorithms import match_algorithm
+from .algorithms.algorithms import get_time_and_cost
 
 PROBLEMS_EUC_2D_PATH = "EUC_2D"
+PROBLEM_ATT_PATH = "ATT/att48.tsp"
 
 
 def run() -> None:
     data: list[list] = []
-    problems_euc_2d = Path(PROBLEMS_EUC_2D_PATH).glob("*")
-    for problem_path in problems_euc_2d:
-        problem = tsplib95.load(problem_path)
-        coords = np.array([problem.node_coords[i] for i in problem.get_nodes()])
-        distances = distance.cdist(coords, coords, "euclidean")
-        graph = nx.Graph(distances)
-        algorithms = ["Christofides", "TATT", "MonteCarlo"]
-        for algorithm in algorithms:
-            start = ti.time()
-            result = match_algorithm(algorithm, graph)
-            end = ti.time()
-            time = end - start
-            if result is not None:
-                cost, _ = result
-                data.append([problem.name, algorithm, time, cost])
+    problems = Path(PROBLEMS_EUC_2D_PATH).glob("*")
+    for problem_path in problems:
+        get_time_and_cost(problem_path, data)
+    get_time_and_cost(Path(PROBLEM_ATT_PATH), data)
     df = pd.DataFrame(data, columns=["Name", "Algorithm", "Time", "Cost"])
     df.to_csv("results.csv", index=False)
