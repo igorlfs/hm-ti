f07a24cd0d7b7b9a721f4286c8135bbf9dc4fd9b
Igor <igorlfs@ufmg.br>
2023-09-28 10:04:52 -0300

    prefira usar launch.json (vs .nvim.lua)

--- .nvim.lua
+++
@@ -1,8 +0,0 @@
-table.insert(require("dap").configurations.python, {
-	type = "python",
-	request = "launch",
-	name = "Module",
-	console = "integratedTerminal",
-	module = "src",
-	cwd = "${workspaceFolder}",
-})
